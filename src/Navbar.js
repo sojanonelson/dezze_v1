4002344e7a4bf25f3de053127176cd20b92823ff
sojanonelson <sojanonelson54@gmail.com>
2023-05-18 18:15:46 +0530

    updates

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex items-center justify-between h-16">
         <div class="flex-shrink-0">
-          <a href='https://google.com' class="text-white text-2xl font-bold">sojan</a>
+          <a href='https://google.com' class="text-white text-2xl font-bold">Dezee/a>
         </div>
         <div class="hidden md:block">
           <div class="ml-10 flex items-baseline space-x-4">
