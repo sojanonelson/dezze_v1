01c5ab08b41866bdbdae37ea03bee06454b5bb99
sojanonelson <sojanonelson54@gmail.com>
2023-05-18 18:10:37 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ const App = () => {
   const [videoid, setVideoid] = useState("");
   const [title, setTitle] = useState("");
   const [link, setLink] = useState("");
+  const [load,setLoad] = useState(false);
+
   const Rapidkey = "a597310939msh2d19f03988866e2p17b2eejsn5e135d6c5603";
   const Rapidhost = "youtube-mp36.p.rapidapi.com";
   const handleChange = (e) => {
     setVideoid(e.target.value);
   };
+  
 
+ 
   const handleSubmit = async (event) => {
     const { id } = getVideoId(`${videoid}`);
     console.log(id);
@@ -29,6 +33,7 @@ const App = () => {
     };
 
     try {
+      setLoad(true)
       const response = await axios.request(options);
       setTitle(response.data.title);
       setLink(response.data.link);
@@ -38,24 +43,26 @@ const App = () => {
   };
 
   return (
-    <div className="main">
-      <h1>YT Audio Download</h1>
-      <div className="input">
-        <input
-          onChange={handleChange}
-          value={videoid}
-          placeholder="Enter video link"
-        />
-        <button onClick={handleSubmit}>Convert</button>
+  <div className="main bg-slate-700 ">
+    
+     <div className="flex flex-col items-center">
+      <div className="input ">
+        <input className="px-4 py-2 w-full border-b-slate-950 rounded-md" onChange={handleChange}
+          value={videoid} type="text" placeholder="Paste the video link here"/>
       </div>
-      <div className="dwn">
-        <h1> {`${title}`} </h1>
-        <a href={`${link}`}>
-          <button>Download</button>
-        </a>
-        <p>{ Rapidhost }</p>
+      <div className="mt-4">
+   <a href={link ? `${link}` : "#"}>
+
+
+        <button className="px-4 py-2 text-white bg-blue-500 rounded-md" onClick={handleSubmit} >{link ? "Download" : "Submit" }</button>
+   </a>
+        
       </div>
+      <h1 className="text-xl text-white-500 font-serif">Video title: {title}</h1>
+      <h1>{load === true && link === "" ? "Loading" : ""}</h1>
+      
     </div>
+  </div>
   );
 };
 
---
+++ src/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+    <nav class="bg-gray-800">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex-shrink-0">
+          <a href='https://google.com' class="text-white text-2xl font-bold">sojan</a>
+        </div>
+        <div class="hidden md:block">
+          <div class="ml-10 flex items-baseline space-x-4">
+            <a  href='https://google.com' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+            <a href='https://google.com' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+            <a  href='https://google.com' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Services</a>
+            <a href='https://github.com/sojanonelson' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  </div>
+  )
+}
+
+export default Navbar
