d5aee7513d98afa94caa750eaf4b581b1dbe0c94
sojanonelson <sojanonelson54@gmail.com>
2023-05-19 23:15:10 +0530

    again update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import axios from "axios";
 import getVideoId from "get-video-id";
 import "./App.css";
@@ -8,6 +8,8 @@ const App = () => {
   const [title, setTitle] = useState("");
   const [link, setLink] = useState("");
   const [load,setLoad] = useState(false);
+  const [adSenseLoaded, setAdSenseLoaded] = useState(false);
+
 
   const Rapidkey = "a597310939msh2d19f03988866e2p17b2eejsn5e135d6c5603";
   const Rapidhost = "youtube-mp36.p.rapidapi.com";
@@ -15,6 +17,18 @@ const App = () => {
     setVideoid(e.target.value);
   };
   
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2285024360288270";
+    script.crossOrigin = "anonymous";
+    script.async = true;
+    script.onload = () => {
+      setAdSenseLoaded(true);
+    };
+  
+    document.body.appendChild(script);
+  }, []);
 
  
   const handleSubmit = async (event) => {
@@ -54,13 +68,24 @@ const App = () => {
    <a href={link ? `${link}` : "#"}>
 
 
-        <button className="px-4 py-2 text-white bg-blue-500 rounded-md" onClick={handleSubmit} >{link ? "Download" : "Submit" }</button>
+        <button className="px-4 py-2 text-white bg-green-500 rounded-md" onClick={handleSubmit} >{link ? "Download" : "Submit" }</button>
    </a>
         
       </div>
-      <h1 className="text-xl text-white-500 font-serif">Video title: {title}</h1>
-      <h1>{load === true && link === "" ? "Loading" : ""}</h1>
       
+      <h1 className="text-xl mt-10 text-lime-200 font-serif">Video title: {title}</h1>
+      <h1>{load === true && link === "" ? "Loading" : ""}</h1>
+
+      {adSenseLoaded && (
+  <ins
+    className="adsbygoogle"
+    style={{ display: "block" }}
+    data-ad-client="pub-2285024360288270"
+    data-ad-slot="f08c47fec0942fa0"
+    data-ad-format="auto"
+    data-full-width-responsive="true"
+  />
+)}
     </div>
   </div>
   );
